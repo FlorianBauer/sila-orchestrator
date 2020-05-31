b24d4dbf23f525a25d8ad6e4ae838bba3c64a830
Florian Bauer <florian.bauer.dev@gmail.com>
2020-05-31 17:55:46 +0200

    Removed unused logger annotation.

--- orchestrator-client/src/main/java/de/fau/clients/orchestrator/feature_explorer/TypeDefLut.java
+++ orchestrator-client/src/main/java/de/fau/clients/orchestrator/feature_explorer/TypeDefLut.java
@@ -2,12 +2,10 @@ package de.fau.clients.orchestrator.feature_explorer;
 
 import java.util.HashMap;
 import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
 import sila_java.library.core.models.DataTypeType;
 import sila_java.library.core.models.Feature;
 import sila_java.library.core.models.SiLAElement;
 
-@Slf4j
 public final class TypeDefLut {
 
     private final HashMap<String, DataTypeType> typeDefElemMap = new HashMap<>();
