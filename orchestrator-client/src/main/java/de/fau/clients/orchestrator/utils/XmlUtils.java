0bcf6d2bf4a1957bca162d79b4410919b19d18a4
Florian Bauer <florian.bauer.dev@gmail.com>
2020-11-01 01:45:51 +0100

    Added function to parse XML encoded SilA Constraints.

--- orchestrator-client/src/main/java/de/fau/clients/orchestrator/utils/XmlUtils.java
+++ orchestrator-client/src/main/java/de/fau/clients/orchestrator/utils/XmlUtils.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import lombok.NonNull;
+import sila_java.library.core.models.Constraints;
 import sila_java.library.core.models.DataTypeType;
 
 /**
@@ -32,5 +33,17 @@ public final class XmlUtils {
     public static DataTypeType parseXmlDataType(@NonNull final String typeAsXml)
             throws JsonProcessingException {
         return xmlMapper.readValue(typeAsXml, DataTypeType.class);
+    }
+
+    /**
+     * Deserializes the given XML string into a SilA <code>Constraints</code> object.
+     *
+     * @param constraintsAsXml The constraints type encoded as XML string.
+     * @return The <code>Constraints</code> object form the unmarshalled XML string on success.
+     * @throws JsonProcessingException
+     */
+    public static Constraints parseXmlConstraints(@NonNull final String constraintsAsXml)
+            throws JsonProcessingException {
+        return xmlMapper.readValue(constraintsAsXml, Constraints.class);
     }
 }
--- orchestrator-client/src/test/java/de/fau/clients/orchestrator/utils/XmlUtilsTest.java
+++ orchestrator-client/src/test/java/de/fau/clients/orchestrator/utils/XmlUtilsTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 import sila_java.library.core.models.BasicType;
+import sila_java.library.core.models.Constraints;
 import sila_java.library.core.models.DataTypeType;
 
 public class XmlUtilsTest {
@@ -95,5 +96,49 @@ public class XmlUtilsTest {
         assertEquals("A", act.getConstrained().getConstraints().getSet().getValue().get(0));
         assertEquals("B", act.getConstrained().getConstraints().getSet().getValue().get(1));
         assertEquals("C", act.getConstrained().getConstraints().getSet().getValue().get(2));
+    }
+
+    @Test
+    void parseXmlConstraints() throws JsonProcessingException {
+        String xmlConStr = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                + "<Constraints>\n"
+                + "    <Set>\n"
+                + "        <Value>pos1</Value>\n"
+                + "        <Value>pos2</Value>\n"
+                + "    </Set>\n"
+                + "</Constraints>";
+        Constraints act = XmlUtils.parseXmlConstraints(xmlConStr);
+        assertNotNull(act.getSet());
+        assertEquals(2, act.getSet().getValue().size());
+        assertEquals("pos1", act.getSet().getValue().get(0));
+        assertEquals("pos2", act.getSet().getValue().get(1));
+
+        xmlConStr = "<Constraints>"
+                + "  <MinimalExclusive>0</MinimalExclusive>"
+                + "  <MaximalExclusive>10</MaximalExclusive>"
+                + "</Constraints>";
+        act = XmlUtils.parseXmlConstraints(xmlConStr);
+        assertEquals("0", act.getMinimalExclusive());
+        assertEquals("10", act.getMaximalExclusive());
+
+        xmlConStr = "<Constraints>\n"
+                + " <Unit>\n"
+                + "     <Label>mL</Label>\n"
+                + "     <Factor>0.000001</Factor>\n"
+                + "     <Offset>0</Offset>\n"
+                + "     <UnitComponent>\n"
+                + "         <SIUnit>Meter</SIUnit>\n"
+                + "         <Exponent>3</Exponent>\n"
+                + "     </UnitComponent>\n"
+                + " </Unit>\n"
+                + "</Constraints>";
+        act = XmlUtils.parseXmlConstraints(xmlConStr);
+        assertNotNull(act.getUnit());
+        assertEquals("mL", act.getUnit().getLabel());
+        assertEquals(0.000001, act.getUnit().getFactor().doubleValue());
+        assertEquals(0.0, act.getUnit().getOffset().doubleValue());
+        assertEquals(1, act.getUnit().getUnitComponent().size());
+        assertEquals("Meter", act.getUnit().getUnitComponent().get(0).getSIUnit());
+        assertEquals(3, act.getUnit().getUnitComponent().get(0).getExponent().intValue());
     }
 }
