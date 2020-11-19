bf213a663bcf7315f959bacc1a761fa622f9c781
Florian Bauer <florian.bauer.dev@gmail.com>
2020-11-19 21:55:18 +0100

    Added utility class to convert basic data types from/to JSON.

---
+++ orchestrator-client/src/main/java/de/fau/clients/orchestrator/utils/SilaBasicTypeUtils.java
@@ -0,0 +1,208 @@
+package de.fau.clients.orchestrator.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Utility class providing functions to put/extract basic data-types to and from JSON. This class
+ * provides basically the same functionality as the classes in the package
+ * <code>sila_java.library.core.sila.types</code> only with the usage of the Jackson
+ * <code>JsonNode</code> type, since the Protobuf complaint <code>SiLAFramework</code>-types are
+ * internally harder to handle.
+ */
+@Slf4j
+public final class SilaBasicTypeUtils {
+
+    public static final String FIELD_VALUE = "value";
+    public static final String FIELD_SECOND = "second";
+    public static final String FIELD_MINUTE = "minute";
+    public static final String FIELD_HOUR = "hour";
+    public static final String FIELD_DAY = "day";
+    public static final String FIELD_MONTH = "month";
+    public static final String FIELD_YEAR = "year";
+    public static final String FIELD_TIMEZONE = "timezone";
+    public static final String FIELD_TZ_HOURS = "hours";
+    public static final String FIELD_TZ_MINUTES = "minutes";
+
+    /**
+     * The mapper used to create <code>JsonNode</code>s.
+     */
+    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
+    private SilaBasicTypeUtils() {
+        throw new UnsupportedOperationException("Instantiation not allowed.");
+    }
+
+    public static JsonNode binaryAsJsonNode(final byte[] val) {
+        final ObjectNode binaryNode = JSON_MAPPER.createObjectNode();
+        binaryNode.put(FIELD_VALUE, val);
+        return binaryNode;
+    }
+
+    public static JsonNode boolAsJsonNode(final boolean val) {
+        final ObjectNode boolNode = JSON_MAPPER.createObjectNode();
+        boolNode.put(FIELD_VALUE, val);
+        return boolNode;
+    }
+
+    public static JsonNode dateAsJsonNode(final OffsetDateTime date) {
+        final ObjectNode dateNode = JSON_MAPPER.createObjectNode();
+        dateNode.put(FIELD_DAY, date.getDayOfMonth());
+        dateNode.put(FIELD_MONTH, date.getMonthValue());
+        dateNode.put(FIELD_YEAR, date.getYear());
+        dateNode.set(FIELD_TIMEZONE, timezoneAsJsonNode(date.getOffset()));
+        return dateNode;
+    }
+
+    public static JsonNode integerAsJsonNode(final long val) {
+        final ObjectNode intNode = JSON_MAPPER.createObjectNode();
+        intNode.put(FIELD_VALUE, val);
+        return intNode;
+    }
+
+    public static JsonNode realAsJsonNode(final double val) {
+        final ObjectNode realNode = JSON_MAPPER.createObjectNode();
+        realNode.put(FIELD_VALUE, val);
+        return realNode;
+    }
+
+    public static JsonNode stringAsJsonNode(final String str) {
+        final ObjectNode strNode = JSON_MAPPER.createObjectNode();
+        strNode.put(FIELD_VALUE, str);
+        return strNode;
+    }
+
+    public static JsonNode timeAsJsonNode(final OffsetTime time) {
+        final ObjectNode timeNode = JSON_MAPPER.createObjectNode();
+        timeNode.put(FIELD_SECOND, time.getSecond());
+        timeNode.put(FIELD_MINUTE, time.getMinute());
+        timeNode.put(FIELD_HOUR, time.getHour());
+        timeNode.set(FIELD_TIMEZONE, timezoneAsJsonNode(time.getOffset()));
+        return timeNode;
+    }
+
+    public static JsonNode timestampAsJsonNode(final OffsetDateTime ts) {
+        final ObjectNode tsNode = JSON_MAPPER.createObjectNode();
+        tsNode.put(FIELD_SECOND, ts.getSecond());
+        tsNode.put(FIELD_MINUTE, ts.getMinute());
+        tsNode.put(FIELD_HOUR, ts.getHour());
+        tsNode.put(FIELD_DAY, ts.getDayOfMonth());
+        tsNode.put(FIELD_MONTH, ts.getMonthValue());
+        tsNode.put(FIELD_YEAR, ts.getYear());
+        tsNode.set(FIELD_TIMEZONE, timezoneAsJsonNode(ts.getOffset()));
+        return tsNode;
+    }
+
+    public static JsonNode timezoneAsJsonNode(final ZoneOffset tz) {
+        final ObjectNode tzNode = JSON_MAPPER.createObjectNode();
+        final int totalSeconds = tz.getTotalSeconds();
+        tzNode.put(FIELD_TZ_HOURS, totalSeconds / 3600);
+        final int offsetMinutes = totalSeconds / 60 % 60;
+        if (offsetMinutes != 0) {
+            tzNode.put(FIELD_TZ_MINUTES, offsetMinutes);
+        }
+        return tzNode;
+    }
+
+    public static LocalDate dateFromJsonNode(final JsonNode jsonNode) {
+        final LocalDate date;
+        if (jsonNode.has(FIELD_VALUE)) {
+            /* This is not conform with SiLA v1.0, so we nag about it in the logs and handle the
+               wrong 'value'-field anyway.*/
+            log.warn("The 'value'-field in Date-types is not conform with the SiLA standard. "
+                    + "Please use the defined data fields instead.");
+            date = DateTimeParser.parseIsoDate(jsonNode.get(FIELD_VALUE).asText());
+        } else {
+            try {
+                date = LocalDate.of(
+                        jsonNode.get(FIELD_YEAR).asInt(),
+                        jsonNode.get(FIELD_MONTH).asInt(),
+                        jsonNode.get(FIELD_DAY).asInt());
+            } catch (final Exception ex) {
+                log.warn(ex.getMessage());
+                return null;
+            }
+        }
+        return date;
+    }
+
+    public static OffsetTime timeFromJsonNode(final JsonNode jsonNode) {
+        final OffsetTime date;
+        if (jsonNode.has(FIELD_VALUE)) {
+            /* This is not conform with SiLA v1.0, so we nag about it in the logs and handle the
+               wrong 'value'-field anyway.*/
+            log.warn("The 'value'-field in Date-types is not conform with the SiLA standard. "
+                    + "Please use the defined data fields instead.");
+            date = DateTimeParser.parseIsoTime(jsonNode.get(FIELD_VALUE).asText());
+        } else {
+            final JsonNode zoneNode = jsonNode.get(FIELD_TIMEZONE);
+            final ZoneOffset zoneOffset = (zoneNode != null)
+                    ? zoneOffsetFromJsonNode(zoneNode)
+                    : ZoneOffset.UTC;
+            try {
+                date = OffsetTime.of(
+                        jsonNode.get(FIELD_HOUR).asInt(),
+                        jsonNode.get(FIELD_MINUTE).asInt(),
+                        jsonNode.get(FIELD_SECOND).asInt(),
+                        0,
+                        zoneOffset);
+            } catch (final Exception ex) {
+                log.warn(ex.getMessage());
+                return null;
+            }
+        }
+        return date;
+    }
+
+    public static OffsetDateTime timestampFromJsonNode(final JsonNode jsonNode) {
+        final OffsetDateTime timestamp;
+        if (jsonNode.has(FIELD_VALUE)) {
+            /* This is not conform with SiLA v1.0, so we nag about it in the logs and handle the
+               wrong 'value'-field anyway.*/
+            log.warn("The 'value'-field in Date-types is not conform with the SiLA standard. "
+                    + "Please use the defined data fields instead.");
+            timestamp = DateTimeParser.parseIsoDateTime(jsonNode.get(FIELD_VALUE).asText());
+        } else {
+            final JsonNode zoneNode = jsonNode.get(FIELD_TIMEZONE);
+            final ZoneOffset zoneOffset = (zoneNode != null)
+                    ? zoneOffsetFromJsonNode(zoneNode)
+                    : ZoneOffset.UTC;
+            try {
+                timestamp = OffsetDateTime.of(
+                        jsonNode.get(FIELD_YEAR).asInt(),
+                        jsonNode.get(FIELD_MONTH).asInt(),
+                        jsonNode.get(FIELD_DAY).asInt(),
+                        jsonNode.get(FIELD_HOUR).asInt(),
+                        jsonNode.get(FIELD_MINUTE).asInt(),
+                        jsonNode.get(FIELD_SECOND).asInt(),
+                        0,
+                        zoneOffset);
+            } catch (final Exception ex) {
+                log.warn(ex.getMessage());
+                return null;
+            }
+        }
+        return timestamp;
+    }
+
+    public static ZoneOffset zoneOffsetFromJsonNode(final JsonNode jsonNode) {
+        final JsonNode hours;
+        final JsonNode minutes;
+        try {
+            hours = jsonNode.get(FIELD_TZ_HOURS);
+            minutes = jsonNode.get(FIELD_TZ_MINUTES);
+        } catch (final Exception ex) {
+            log.warn("Invalid ZoneOffset. Zone is set to UTC.");
+            return ZoneOffset.UTC;
+        }
+        return ZoneOffset.ofHoursMinutes(
+                hours != null ? hours.asInt(0) : 0,
+                minutes != null ? minutes.asInt(0) : 0);
+    }
+}
---
+++ orchestrator-client/src/test/java/de/fau/clients/orchestrator/utils/SilaBasciTypeUtilsTest.java
@@ -0,0 +1,214 @@
+package de.fau.clients.orchestrator.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import static de.fau.clients.orchestrator.utils.SilaBasicTypeUtils.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import sila_java.library.core.sila.types.SiLABinary;
+import sila_java.library.core.sila.types.SiLAString;
+import sila_java.library.core.sila.types.SiLATime;
+import sila_java.library.core.sila.types.SiLATimeZone;
+import sila_java.library.core.sila.types.SiLATimestamp;
+
+public class SilaBasciTypeUtilsTest {
+
+    private final ObjectMapper jsonMapper = new ObjectMapper();
+
+    @Test
+    public void binaryAsJsonNode() {
+        final byte[] data = {(byte) 0x01, (byte) 0x02, (byte) 0x03};
+        JsonNode actNode = SilaBasicTypeUtils.binaryAsJsonNode(data);
+        try {
+            assertArrayEquals(data, actNode.get(FIELD_VALUE).binaryValue());
+        } catch (final IOException ex) {
+            fail(ex.getMessage());
+        }
+
+        try {
+            String exp = JsonFormat.printer().print(SiLABinary.fromBytes(data)).replaceAll("\\s+", "");
+            String act = actNode.toString().replaceAll("\\s+", "");
+            assertEquals(exp, act);
+        } catch (final InvalidProtocolBufferException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void boolAsJsonNode() {
+        JsonNode actNode = SilaBasicTypeUtils.boolAsJsonNode(true);
+        assertEquals(true, actNode.get(FIELD_VALUE).booleanValue());
+        actNode = SilaBasicTypeUtils.boolAsJsonNode(false);
+        assertEquals(false, actNode.get(FIELD_VALUE).booleanValue());
+        String act = actNode.toString().replaceAll("\\s+", "");
+        assertEquals("{\"value\":false}", act);
+    }
+
+    @Test
+    public void dateAsJsonNode() {
+        final OffsetDateTime date = OffsetDateTime.of(2020, 11, 10, 0, 0, 0, 0, ZoneOffset.ofHours(3));
+        JsonNode actNode = SilaBasicTypeUtils.dateAsJsonNode(date);
+        assertEquals(date.getDayOfMonth(), actNode.get(FIELD_DAY).intValue());
+        assertEquals(date.getMonthValue(), actNode.get(FIELD_MONTH).intValue());
+        assertEquals(date.getYear(), actNode.get(FIELD_YEAR).intValue());
+        String act = actNode.toString().replaceAll("\\s+", "");
+        assertEquals("{\"day\":10,\"month\":11,\"year\":2020,\"timezone\":{\"hours\":3}}", act);
+    }
+
+    @Test
+    public void integerAsJsonNode() {
+        JsonNode actNode = SilaBasicTypeUtils.integerAsJsonNode(42);
+        assertEquals(42, actNode.get(FIELD_VALUE).intValue());
+        actNode = SilaBasicTypeUtils.integerAsJsonNode(-13);
+        assertEquals(-13, actNode.get(FIELD_VALUE).intValue());
+        String act = actNode.toString().replaceAll("\\s+", "");
+        assertEquals("{\"value\":-13}", act);
+    }
+
+    @Test
+    public void realAsJsonNode() {
+        JsonNode actNode = SilaBasicTypeUtils.realAsJsonNode(3.141593);
+        assertEquals(3.141593, actNode.get(FIELD_VALUE).doubleValue());
+        actNode = SilaBasicTypeUtils.realAsJsonNode(-13.5);
+        assertEquals(-13.5, actNode.get(FIELD_VALUE).doubleValue());
+        String act = actNode.toString().replaceAll("\\s+", "");
+        assertEquals("{\"value\":-13.5}", act);
+    }
+
+    @Test
+    public void stringAsJsonNode() {
+        JsonNode actNode = SilaBasicTypeUtils.stringAsJsonNode("Lorem ipsum dolor sit amet");
+        assertEquals("Lorem ipsum dolor sit amet", actNode.get(FIELD_VALUE).asText());
+        final String unwantedStr = "\"äöü\\n\\tß\\\\0xf00bar:<></>\\\";€¶@æ²³\\b01101®testâ€™\\\\u0048{[]}\\u2212\"¯\\_(ツ)_/¯;";
+        actNode = SilaBasicTypeUtils.stringAsJsonNode(unwantedStr);
+        assertEquals(unwantedStr, actNode.get(FIELD_VALUE).asText());
+
+        try {
+            String exp = JsonFormat.printer().print(SiLAString.from(unwantedStr)).replaceAll("\\s+", "");
+            String act = actNode.toString().replaceAll("\\s+", "");
+            assertEquals(exp, act);
+        } catch (final InvalidProtocolBufferException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void timeAsJsonNode() {
+        final OffsetTime time = OffsetTime.of(9, 8, 7, 0, ZoneOffset.ofHoursMinutes(3, 45));
+        JsonNode actNode = SilaBasicTypeUtils.timeAsJsonNode(time);
+        assertEquals(time.getSecond(), actNode.get(FIELD_SECOND).intValue());
+        assertEquals(time.getMinute(), actNode.get(FIELD_MINUTE).intValue());
+        assertEquals(time.getHour(), actNode.get(FIELD_HOUR).intValue());
+        String act = actNode.toString().replaceAll("\\s+", "");
+        assertEquals("{\"second\":7,\"minute\":8,\"hour\":9,\"timezone\":{\"hours\":3,\"minutes\":45}}", act);
+
+        try {
+            String exp = JsonFormat.printer().print(SiLATime.from(time)).replaceAll("\\s+", "");
+            assertEquals(exp, act);
+        } catch (final InvalidProtocolBufferException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void timestampAsJsonNode() {
+        final OffsetDateTime ts = OffsetDateTime.of(2020, 11, 10, 9, 8, 7, 0, ZoneOffset.ofHoursMinutes(3, 45));
+        JsonNode actNode = SilaBasicTypeUtils.timestampAsJsonNode(ts);
+        assertEquals(ts.getSecond(), actNode.get(FIELD_SECOND).intValue());
+        assertEquals(ts.getMinute(), actNode.get(FIELD_MINUTE).intValue());
+        assertEquals(ts.getHour(), actNode.get(FIELD_HOUR).intValue());
+        assertEquals(ts.getDayOfMonth(), actNode.get(FIELD_DAY).intValue());
+        assertEquals(ts.getMonthValue(), actNode.get(FIELD_MONTH).intValue());
+        assertEquals(ts.getYear(), actNode.get(FIELD_YEAR).intValue());
+        String act = actNode.toString().replaceAll("\\s+", "");
+        assertEquals("{\"second\":7,\"minute\":8,\"hour\":9,\"day\":10,\"month\":11,\"year\":2020,"
+                + "\"timezone\":{\"hours\":3,\"minutes\":45}}", act);
+
+        try {
+            String exp = JsonFormat.printer().print(SiLATimestamp.from(ts)).replaceAll("\\s+", "");
+            assertEquals(exp, act);
+        } catch (final InvalidProtocolBufferException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void timezoneAsJsonNode() {
+        final ZoneOffset zone = ZoneOffset.ofHoursMinutes(12, 15);
+        JsonNode actNode = SilaBasicTypeUtils.timezoneAsJsonNode(zone);
+        assertEquals(zone.getTotalSeconds() / 3600, actNode.get(FIELD_TZ_HOURS).intValue());
+        assertEquals((zone.getTotalSeconds() / 60) % 60, actNode.get(FIELD_TZ_MINUTES).intValue());
+        String act = actNode.toString().replaceAll("\\s+", "");
+        assertEquals("{\"hours\":12,\"minutes\":15}", act);
+        try {
+            String exp = JsonFormat.printer().print(SiLATimeZone.from(zone)).replaceAll("\\s+", "");
+            assertEquals(exp, act);
+        } catch (final InvalidProtocolBufferException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void dateFromJsonNode() {
+        ObjectNode node = jsonMapper.createObjectNode();
+        node.put(FIELD_YEAR, 2020);
+        node.put(FIELD_MONTH, 11);
+        node.put(FIELD_DAY, 10);
+
+        final LocalDate exp = LocalDate.of(2020, 11, 10);
+        LocalDate act = SilaBasicTypeUtils.dateFromJsonNode(node);
+        assertEquals(exp, act);
+    }
+
+    @Test
+    public void timeFromJsonNode() throws JsonProcessingException {
+        JsonNode node = jsonMapper.readTree("{\"second\":7,\"minute\":8,\"hour\":9,\""
+                + "timezone\":{\"hours\":3,\"minutes\":45}}");
+
+        OffsetTime exp = OffsetTime.of(9, 8, 7, 0, ZoneOffset.ofHoursMinutes(3, 45));
+        OffsetTime act = SilaBasicTypeUtils.timeFromJsonNode(node);
+        assertEquals(exp, act);
+
+        node = jsonMapper.readTree("{\"second\":1,\"minute\":2,\"hour\":3}");
+        exp = OffsetTime.of(3, 2, 1, 0, ZoneOffset.UTC);
+        act = SilaBasicTypeUtils.timeFromJsonNode(node);
+        assertEquals(exp, act);
+    }
+
+    @Test
+    public void timestampFromJsonNode() throws JsonProcessingException {
+        JsonNode node = jsonMapper.readTree("{\"second\":7,\"minute\":8,\"hour\":9,\"day\":10,"
+                + "\"month\":11,\"year\":2020,\"timezone\":{\"hours\":3,\"minutes\":45}}");
+
+        final OffsetDateTime exp = OffsetDateTime.of(2020, 11, 10, 9, 8, 7, 0, ZoneOffset.ofHoursMinutes(3, 45));
+        OffsetDateTime act = SilaBasicTypeUtils.timestampFromJsonNode(node);
+        assertEquals(exp, act);
+    }
+
+    @Test
+    public void zoneOffsetFromJsonNode() throws JsonProcessingException {
+        JsonNode node = jsonMapper.readTree("{\"hours\":12,\"minutes\":45}");
+        ZoneOffset exp = ZoneOffset.ofHoursMinutes(12, 45);
+        ZoneOffset act = SilaBasicTypeUtils.zoneOffsetFromJsonNode(node);
+        assertEquals(exp, act);
+        node = jsonMapper.readTree("{\"hours\":2}");
+        exp = ZoneOffset.ofHours(2);
+        act = SilaBasicTypeUtils.zoneOffsetFromJsonNode(node);
+        assertEquals(exp, act);
+        node = jsonMapper.readTree("{\"hours\":11,\"minutes\":12,\"seconds\":13}");
+        exp = ZoneOffset.ofHoursMinutesSeconds(11, 12, 0);
+        act = SilaBasicTypeUtils.zoneOffsetFromJsonNode(node);
+        assertEquals(exp, act);
+    }
+}
